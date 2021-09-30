a0d4d8add43246dd9f60fbbe76b1c1409dfc444a
CarlosGenis <carlosgenis06@gmail.com>
2021-09-30 19:22:12 -0400

    worked on foreach function

--- assignment2.js
+++ assignment2.js
@@ -1,6 +1,13 @@
 // FOR EACH //
-Array.prototype.myEach = function() {
+Array.prototype.myEach = function(callbackFn) {
+    for (let i = 0; i < this.length; i++){
+        if (this[i] === undefined ) continue;
+        //element
+        //element index 
+        //element index array
+        callbackFn(this[i], i, this);
 
+    }
 };
 
 // MAP //
