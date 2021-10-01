9cc5cca430e4c220d36531cb04c86f9a18d3fcb7
KevinFernandez16 <kjfernandez1456@gmail.com>
2021-09-30 22:26:32 -0400

    created Mymap function

--- assignment2.js
+++ assignment2.js
@@ -4,9 +4,18 @@ Array.prototype.myEach = function() {
 };
 
 // MAP //
-Array.prototype.myMap = function() {
-
+let array = [2, 4, 6, 8, 10, 12];// example array
+Array.prototype.myMap = function(print_array){
+    let array_brac = [];
+    let array_size = this.length;// set a variable equal to the array's length.
+    for (let i = 0; i < array_size; i++) {
+        let counter = print_array(this[i]);
+        array.push(counter);
+    }
+    return array;
 };
+array = array.myMap(e => e * 2);//multiplies array values by 2
+console.log(array)//prints out the array
 
 // FILTER //
 Array.prototype.myFilter = function() {
