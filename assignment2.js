88d6bf86a958f9bde1ef64a414dd650da840ceb5
KevinFernandez16 <kjfernandez1456@gmail.com>
2021-10-03 18:43:30 -0400

    created MyIndexOf function

--- assignment2.js
+++ assignment2.js
@@ -34,9 +34,19 @@ Array.prototype.myIncludes = function() {
 };
 
 // INDEXOF //
-Array.prototype.myIndexOf = function() {
-
+const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];//example array
+Array.prototype.myIndexOf = function (searchElement, element) {
+    let animal = element//set argument to variable animal
+    for (let i = 0 || animal; i < this.length; i++) {
+        if (this[i] === searchElement) {
+            return i || animal;//returns position the animal is located in in the array
+        }
+    }
+    return -1;//returns -1 if false
 };
+console.log(beasts.myIndexOf('bison'));// should output 1
+console.log(beasts.myIndexOf('bison', 2));// should output 4
+console.log(beasts.myIndexOf('giraffe'));// should output -1
 
 // PUSH //
 Array.prototype.myPush = function() {
