d026f5a2dda151d8c6495cae4b1f787b478cb83b
CarlosGenis <carlosgenis06@gmail.com>
2021-10-03 15:22:41 -0400

    added push function

--- assignment2.js
+++ assignment2.js
@@ -105,8 +105,23 @@ Array.prototype.myIndexOf = function() {
 
 // PUSH //
 Array.prototype.myPush = function() {
-
-};
+    for(let i = 0; i < arguments.length; i++)
+    //adding the element to the end of the array
+      this[this.length] = arguments[i];
+    return this.length;
+  };
+  
+  //const animals = ['pigs', 'goats', 'sheep'];
+  
+  //const count = animals.myPush('cows');
+  //console.log(count);
+  // expected output: 4
+  //console.log(animals);
+  // expected output: Array ["pigs", "goats", "sheep", "cows"]
+  
+  //animals.myPush('chickens', 'cats', 'dogs');
+  //console.log(animals);
+  // expected output: Array ["pigs", "goats", "sheep", "cows", "chickens", "cats", "dogs"]
 
 // LASTINDEXOF //
 Array.prototype.myLastIndexOf = function() {
